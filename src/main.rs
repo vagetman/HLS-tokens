9d333027880f31378187bd1786ec0d87f05fedd2
Vadim Getmanshchuk <vagetman@gmail.com>
2021-03-10 16:06:21 -0800

    upgraded to 0.7.0 fastly crate

--- src/main.rs
+++ src/main.rs
@@ -1,105 +1,78 @@
-use fastly::http::{HeaderValue, Method, StatusCode};
+use fastly::http::{Method, StatusCode, header};
 use fastly::http::header::{CONTENT_LENGTH, HOST};
-use fastly::http::response::Parts;
-use fastly::{downstream_request, Body, Error, Request, RequestExt, Response, ResponseExt};
-
-use std::convert::{TryFrom};
+use fastly::{Error, Request, Response};
 use std::io::Write;
 
 use m3u8_rs::playlist::{Playlist, MasterPlaylist, MediaPlaylist};
 
-const VALID_METHODS: [Method; 3] = [
-    Method::HEAD,
-    Method::GET,
-    Method::POST
-];
-
-/// Handle the downstream request from the client.
-///
-/// This function accepts a Request<Body> and returns a Response<Body>. It could
-/// be used to route based on the request properties (such as method or path),
-/// send the request to a backend, make completely new requests and/or generate
-/// synthetic responses.
-fn handle_request(mut req: Request<Body>) -> Result<Response<Body>, Error> {
-
-    if !(VALID_METHODS.contains(req.method())) {
-        return Ok(Response::builder()
-            .status(StatusCode::METHOD_NOT_ALLOWED)
-            .body(Body::try_from("This method is not allowed")?)?);
-    }
-
-    // Pattern match on the request method and path.
-//    match (req.method(), req.uri().path()) {
-//        (&Method::GET, path) if path.ends_with("m3u8") => {
-    match req.method() {
-        &Method::GET => {
-
-            // Just set the TTL cache override
-            req.set_pass();
-
-            Ok(req.send("dai-backend")?)
-        }
-
-        // Catch all other requests and return a 404.
-        _ => Ok(Response::builder()
-            .status(StatusCode::NOT_FOUND)
-            .body(Body::try_from("The page you requested could not be found")?)?),
-    }
-}
+/// This should be changed to match the name of your own backend. See the the `Hosts` section of
+/// the Fastly WASM service UI for more information.
+const BACKEND_NAME: &str = "backend_name";
 
 /// The entrypoint for your application
 ///
 /// This function is triggered when your service receives a client request, and
 /// should ultimately call `send_downstream` on a fastly::Response to deliver an
 /// HTTP response to the client.
-fn main() -> Result<(), Error> {
+#[fastly::main]
+fn main(req: Request) -> Result<Response, Error> {
     // set up logging first
     fastly::log::set_panic_endpoint("mylogs").unwrap();
 
-    // get request from downstream
-    let req = downstream_request();
+    // Filter request methods...
+    match req.get_method() {
+        // Allow GET and HEAD requests.
+        &Method::GET | &Method::HEAD => (),
+
+        // Accept PURGE requests; it does not matter to which backend they are sent.
+        m if m == "PURGE" => return Ok(req.send(BACKEND_NAME)?),
+
+        // Deny anything else.
+        _ => {
+            return Ok(Response::from_status(StatusCode::METHOD_NOT_ALLOWED)
+                .with_header(header::ALLOW, "GET, HEAD")
+                .with_body_text_html("This method is not allowed\n"))
+        }
+    };
 
     // we need to keep req path to build ACL if manifest has relative paths
-    let req_url_path = req.uri().path().to_string();
+    let req_url_path = req.get_path().to_string();
 
     let mut endpoint = fastly::log::Endpoint::from_name("mylogs");
 
-    match handle_request(req) {
-        Ok(beresp) if beresp.status().is_success() => {
-            let (beresp_meta, beresp_body) = beresp.into_parts();
-            let bytes = beresp_body.into_bytes();
+    match req.send(BACKEND_NAME) {
+        Ok(mut beresp) if beresp.get_status().is_success() => {
 
-            match m3u8_rs::parse_playlist_res(&bytes) {
-                Ok(Playlist::MasterPlaylist(mut pl)) => master_token(&mut pl, beresp_meta, req_url_path)?,
-                Ok(Playlist::MediaPlaylist(mut pl)) => media_token(&mut pl, beresp_meta, req_url_path)?,
+            // consume the original response body
+            let bytes = beresp.take_body_bytes();
+
+            let resp = match m3u8_rs::parse_playlist_res(&bytes) {
+                Ok(Playlist::MasterPlaylist(mut pl)) => master_token(&mut pl, beresp, req_url_path)?,
+                Ok(Playlist::MediaPlaylist(mut pl)) => media_token(&mut pl, beresp, req_url_path)?,
                 Err(_) => {
-                    let resp = Response::builder()
-                        .status(StatusCode::METHOD_NOT_ALLOWED)
-                        .body(Body::try_from("Oopsy-daisy")?)?;
-                    resp.send_downstream_streaming();
+                    Response::from_status(StatusCode::INTERNAL_SERVER_ERROR)
+                        .with_body("The manifest is not recognized")
                 }
-            }
+            };
+            Ok(resp)
         }
 
-        Ok(beresp) => {
-            writeln!(endpoint, "req_url_path - {}", req_url_path).unwrap();
-            writeln!(endpoint, "Didn't work").unwrap();
-            beresp.send_downstream();
+        Ok(resp) => {
+            writeln!(endpoint, "{} path didn't work", req_url_path).unwrap();
+            Ok(resp)
         }
 
         Err(e) => {
-            writeln!(endpoint, "Err(e) - {}", e).unwrap();
-            let mut resp = Response::new(e.to_string());
-            *resp.status_mut() = StatusCode::INTERNAL_SERVER_ERROR;
-            resp.send_downstream();
+            writeln!(endpoint, "Err(e) - {:?}", e).unwrap();
+            let resp = Response::from_status(StatusCode::INTERNAL_SERVER_ERROR);
+            Ok(resp)
         }
     }
-    Ok(())
 }
 
 fn master_token(pl: &mut MasterPlaylist,
-                mut meta: Parts,
-                req_url_path : String) -> Result<(), Error> {
+                mut beresp: Response,
+                req_url_path : String) -> Result<Response, Error> {
 
     let now = get_epoch();
     for varient in &mut pl.variants {
@@ -109,22 +82,14 @@ fn master_token(pl: &mut MasterPlaylist,
 
     pl.write_to(&mut resp_body).unwrap();
 
-    let resp_http_content_length: String = resp_body.len().to_string();
+    beresp.set_body(resp_body);
 
-    meta.headers.remove(CONTENT_LENGTH);
-    meta.headers.insert(CONTENT_LENGTH,
-                        HeaderValue::from_str(&resp_http_content_length).unwrap());
-
-    let resp = Response::from_parts(meta, resp_body);
-
-    resp.send_downstream();
-
-    Ok(())
+    Ok(beresp)
 }
 
 fn media_token(pl: &mut MediaPlaylist,
-               mut meta: Parts,
-               req_url_path : String) -> Result<(), Error>  {
+               mut resp: Response,
+               req_url_path : String) -> Result<Response, Error>  {
 
     let now = get_epoch();
     for segment in &mut pl.segments {
@@ -141,17 +106,9 @@ fn media_token(pl: &mut MediaPlaylist,
 
     pl.write_to(&mut resp_body).unwrap();
 
-    let resp_http_content_length: String = resp_body.len().to_string();
+    resp.set_body(resp_body);
 
-    meta.headers.remove(CONTENT_LENGTH);
-    meta.headers.insert(CONTENT_LENGTH,
-                        HeaderValue::from_str(&resp_http_content_length).unwrap());
-
-    let resp = Response::from_parts(meta, resp_body);
-
-    resp.send_downstream();
-
-    Ok(())
+    Ok(resp)
 }
 
 fn get_epoch() ->u64 {
@@ -183,7 +140,7 @@ fn set_token(unix_time: u64, url: String, req_url_path: &str) -> String {
     }
 
     let uri_path = format!("{}{}", dirname, basename);
-    let mut acl;
+    let acl;
 
     // if the path is not relative
     // we need to add the working directory from the _this_ request
@@ -234,7 +191,7 @@ use regex::Regex;
 fn get_parts(url: &str) -> (String, String, String, String, String) {
     lazy_static! {
         static ref RE: Regex =
-        Regex::new(r"^(?:(?P<proto>(?:http|https)://)(?P<host>[^/]+))?(?P<dirname>/?.*?/)?(?P<basename>[^/]+?)?(?P<qs>\?.*)?$").unwrap();
+        Regex::new(r"^(?:(?P<proto>(?:https?)://)(?P<host>[^/]+))?(?P<dirname>/?.*?/)?(?P<basename>[^/]+?)?(?P<qs>\?.*)?$").unwrap();
     }
     let caps = RE.captures(url).unwrap();
 
